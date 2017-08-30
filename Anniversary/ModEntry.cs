7df7fbfefcd8404a3b221daf5781b7f293003fab
githubschman <sarahhubschmail@gmail.com>
2017-08-29 20:42:10 -0400

    added mail file

--- Anniversary/ModEntry.cs
+++ Anniversary/ModEntry.cs
@@ -24,38 +24,25 @@ namespace Anniversary
 			public int AnniDay { get; set; }
 			public string AnniSeason { get; set; }
 			public string KeyBinding { get; set; } = "O";
-   
 		}
 
 		public void Event_AfterDayStarted(object sender, EventArgs e)
 		{
 
-			//DELETE THIS! JUST FOR TESTING!!!!
-			// //////////////////////////////
-			Game1.mailbox.Enqueue("AnniversaryTomorrow1");
-			Game1.mailbox.Enqueue("AnniversaryTomorrow2");
-			Game1.mailbox.Enqueue("AnniversaryTomorrow3");
-			Game1.mailbox.Enqueue("AnniversaryTomorrow4");
-			Game1.mailbox.Enqueue("Robin");
-			this.Config.AnniDay = 16;
-			this.Config.AnniSeason = "spring";
-			this.Helper.WriteJsonFile<ModConfig>(this.DataFilePath, new ModConfig());
-			// /////////////////////////////
-
-			this.Monitor.Log(this.Config.AnniDay + this.Config.AnniSeason + Game1.dayOfMonth + Game1.currentSeason);
-
+			// Get Wedding Anniversary Date (and other necessary info)
 			String[] Seasons = {"spring", "summer", "fall", "winter"};
 			int daysMarried = Game1.player.daysMarried;
 			float years = daysMarried / 112;
 			double yearsMarried = Math.Floor(years);
-			int index = 0;
+
+			// Message Information
 			string favething = Game1.player.favoriteThing;
-			
 			string spouse = Game1.player.spouse;
 			int numChildren = Game1.player.getNumberOfChildren();
 
 			// Calculate the player's wedding Season and Day. It will write this to a JSON file (config.json), so the mod will only need to calculate once per player.
-			// Players can also modify config.json data if they want to
+			// Players can also modify config.json data if they want to hardcode their anniversary date!
+			int index = 0;
 			if(this.Config.AnniDay == 0 && daysMarried > 1){
 				
 				if(Game1.currentSeason == "summer") index=1;
@@ -64,38 +51,33 @@ namespace Anniversary
 				
 				int day = daysMarried - 0;
 
-				while(day > 28){
+				while(day > 28){ // get years % daysmarried and the month by index
 					index--;
-					if(index < 0){
-						index = 3;
-					}
+					if(index < 0) index = 3;
 					day -= 28;
 				}
 					
+				// accounts for the remainder days
 				if(index > 0) index = index - 1;
 				else index = 3;
 
-				this.Monitor.Log("anniversary date SHOULD be set to : " + Seasons[index] + day);
-				// add this back in for production:
-				// this.Config.AnniDay = day;
-				// this.Config.AnniSeason = Seasons[index];
+				this.Config.AnniDay = day;
+				this.Config.AnniSeason = Seasons[index];
 
-				// this.Helper.WriteJsonFile<ModConfig>(this.DataFilePath, new ModConfig());
-				// this.Monitor.Log("anniversary date was saved to: " + this.Config.AnniDay + " of " + this.Config.AnniSeason);
-			}
+				this.Helper.WriteJsonFile<ModConfig>(this.DataFilePath, new ModConfig());
+				this.Monitor.Log("anniversary date was saved to: " + this.Config.AnniDay + " of " + this.Config.AnniSeason);
 
+			}	
 
-
-
-			// Day Before Anniversary:
+			// Day Before Anniversary
 			int dayBefore = this.Config.AnniDay - 1;
 			if(dayBefore < 1) dayBefore = 28;
 			int indexBefore = index-1;
 			if(indexBefore < 0) index = 3;
-
-			string monthBefore = dayBefore == 28 ? Seasons[indexBefore] : Game1.currentSeason;
 			
-
+			string monthBefore = Game1.currentSeason;
+			if(dayBefore == 28) monthBefore = Seasons[indexBefore];
+			
 			if(Game1.dayOfMonth == dayBefore && Game1.currentSeason == monthBefore){
 
 				// Your spouse will tell you to check the mail: 
@@ -110,12 +92,11 @@ namespace Anniversary
 			}
 
 			
-			this.Monitor.Log(this.Config.AnniDay + this.Config.AnniSeason);
-			// If they are married, and already have an anniversary set, check to see if anniversary is today:
 			if(daysMarried > 1 && this.Config.AnniDay >= 1){
-
+				// Check to see if the couple's anniversary is today
 				if(this.Config.AnniDay == Game1.dayOfMonth && this.Config.AnniSeason == Game1.currentSeason){
-					eventTrigger = true;
+					eventTrigger = true; // triggers wedding music to play when you enter town
+					// Custom anniversary message to be delivered by your spouse
 					string sweetTalk = Game1.player.name + ", I can't believe we've been married for " + yearsMarried + (yearsMarried > 1 ? " years" : " year") + " or " + daysMarried + " days. Yes, I've been keeping count! Every day with you is so special. And I know you love me more than you love " + favething + "! You are the greatest " + (Game1.player.IsMale ? "husband" : "wife") + (numChildren < 1 ? " in Stardew Valley!" : " and parent!$l");
 					Dialogue happyAnniversaryBabe = new Dialogue(sweetTalk, Game1.player.getSpouse());
 					Game1.player.getSpouse().CurrentDialogue.Push(happyAnniversaryBabe);
--- README.md
+++ README.md
@@ -4,18 +4,19 @@
 
 ## How to Play
 
+Step One:
 Get the latest version of SMAPI, an open-source modding API for Stardew Valley 
 ```
 https://github.com/Pathoschild/SMAPI/releases
 ```
 
-Step One: Add Anniversary to your mods folder:
+Step Two: Add Anniversary to your mods folder:
 ```
 cd (... filepath)/Stardew\ Valley/Contents/MacOS/Mods
 git clone https://github.com/githubschman/Anniversary
 ```
 
-Step Two: Replace your Mail.xnb file with the one provided in this mod. The path should look like this:
+Step Three: Replace your mail.xnb file with the one provided in this mod. The path should look like this:
 
 ```
 yourname/library/applicationsupport/steam/steamapps/common/stardewvalley/contents/resources/content/data/mail.xnb
@@ -26,7 +27,10 @@ I included the unpacked mail.yaml file in this repo as well, if you wanted to ta
 
 ## About the Mod:
 
-If you are married before you download this mod, don't worry! Your anniversary date will be calculated based off of the current day, and how many days you've been married. If you get married while this mod is installed, your wedding day will be your anniversary. Once your anniversary date is calculate, it will be written to the mod's config.json file. Feel free to edit this file for testing, or if you just want to make your anniversary come a bit sooner! ;) 
+If you are married before you download this mod, don't worry! Your anniversary date will be calculated based off of the current day, and how many days you've been married. If you get married while this mod is installed, your wedding day will be your anniversary. 
+
+
+Once your anniversary date is calculated, it will be written to the mod's config.json file. Feel free to edit this file for testing, or if you just want to make your anniversary come a bit sooner! ;) 
 
 
 On the day before your anniversary, a letter will be queued to your mailbox from your spouse (which is weird because you live together, but whatever) containing a special anniversary present. On the day of your anniversary, your spouse will say a sweet message, based off of how many years you've been married, whether you have children, and what you specified as your "favorite thing" in the beginning of the game. 
